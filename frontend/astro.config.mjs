6d164a4915fc172c148bad62622a3de4d9bacf0d
pimphand <faisaldwiki69@gmail.com>
2025-06-19 23:27:49 +0700

    chore: remove all project files and configurations
    - Deleted all source files, including components, pages, and assets.
    - Removed configuration files such as package.json, .gitignore, and tailwind.config.mjs.
    - Cleared out public assets including images and SVGs.
    - This commit effectively resets the project to a clean state.

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -3,7 +3,10 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind()],
+  adapter: netlify()
 });
